b153dcaf235ca3a70e08e35d63d54ff384ea2978
sangjin-hash <sangjin970301@gmail.com>
2025-12-05 21:16:38 +0900

    Initial Commit

---
+++ ToyVideoStreamingPlayerTests/ToyVideoStreamingPlayerTests.swift
@@ -0,0 +1,17 @@
+//
+//  ToyVideoStreamingPlayerTests.swift
+//  ToyVideoStreamingPlayerTests
+//
+//  Created by 이상진 on 12/5/25.
+//
+
+import Testing
+@testable import ToyVideoStreamingPlayer
+
+struct ToyVideoStreamingPlayerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
